c0b113f020f896033211eb3223da5cf04e96ea7d
g0b0t <alekskro12@gmail.com>
2025-10-21 18:03:53 +0300

    Chage structures project

--- hookah-split-twa/vite.config.ts
+++ hookah-split-twa/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',           // ВАЖНО: деплой через Actions → базовый путь = '/'
   plugins: [react()],
 })
